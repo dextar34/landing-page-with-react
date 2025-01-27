7bb65f4387ea2ef5f0abdf04c960eca44db806b8
dextar34 <weknownothingbutheknows23@gmail.com>
2025-01-27 15:03:50 +0600

    project created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl bg-emerald-300  p-4 font-play-india">
+        Whereas recognition of the inherent dignity
+      </h1>
+      
+    </div>  
+  );
+};
+
+export default App;
